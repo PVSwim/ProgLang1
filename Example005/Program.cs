6b67a082dcde38676179697dcafe7ed90c6118bc
PVSwim <pavel.vlasenko65@gmail.com>
2022-12-13 22:29:18 +0300

    Hi Masha

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Input your name: ");
+string UserName=Console.ReadLine();
+if (UserName == "Masha")
+{
+Console.Write("Hi: " + UserName);
+}
+else
+{
+    Console.Write("Hello: " + UserName);
+}
+
